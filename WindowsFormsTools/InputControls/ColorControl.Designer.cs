b6d5dddae5f337353bf3a88d0d27046c5d103702
Ulan Akmatbekov <akmatbekov.swe@proton.me>
2025-02-24 22:02:08 +0100

    Add color picker control

---
+++ WindowsFormsTools/InputControls/ColorControl.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsTools.InputControls
+{
+    partial class ColorControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorPanel = new System.Windows.Forms.Panel();
+            this.colorLabel = new System.Windows.Forms.Label();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // colorPanel
+            // 
+            this.colorPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.colorPanel.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.colorPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorPanel.Location = new System.Drawing.Point(69, 2);
+            this.colorPanel.Name = "colorPanel";
+            this.colorPanel.Size = new System.Drawing.Size(80, 17);
+            this.colorPanel.TabIndex = 3;
+            this.colorPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.colorPanel_MouseClick);
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.AutoSize = true;
+            this.colorLabel.Location = new System.Drawing.Point(-3, 3);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(31, 13);
+            this.colorLabel.TabIndex = 2;
+            this.colorLabel.Text = "Color";
+            // 
+            // ColorControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.colorPanel);
+            this.Controls.Add(this.colorLabel);
+            this.Name = "ColorControl";
+            this.Size = new System.Drawing.Size(149, 23);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel colorPanel;
+        private System.Windows.Forms.Label colorLabel;
+        private System.Windows.Forms.ColorDialog colorDialog;
+    }
+}
---
+++ WindowsFormsTools/InputControls/ColorControl.cs
@@ -0,0 +1,47 @@
+﻿
+namespace WindowsFormsTools.InputControls
+{
+    public partial class ColorControl : UserControl
+    {
+        public event EventHandler<ValueChangedEventArgs<Color>>? ValueChanged;
+
+        public string Header
+        {
+            get => colorLabel.Text;
+            set => colorLabel.Text = value;
+        }
+
+        public Color Value
+        {
+            get => colorPanel.BackColor;
+            set
+            {
+                colorPanel.BackColor = value;
+                ValueChanged?.Invoke(this, new ValueChangedEventArgs<Color>(value));
+            }
+        }
+
+        public int ColorPanelWidth
+        {
+            get => colorPanel.Width;
+            set
+            {
+                colorPanel.Location = new Point(Size.Width - value, colorPanel.Location.Y);
+                colorPanel.Width = value;
+            }
+        }
+
+        public ColorControl()
+        {
+            InitializeComponent();
+        }
+
+        private void colorPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                Value = colorDialog.Color;
+            }
+        }
+    }
+}
